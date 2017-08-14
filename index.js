78cd1f4d8ea34b3bb6635e35b687a15f38b4e72d
Will Holcomb <will@dhappy.org>
2017-08-14 17:35:19 -0400

    yarn add angular-ui-router

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-angular.module('eventTypes', ['ngMaterial'])
+angular.module('eventTypes', ['ngMaterial', 'ui.router'])
+    .config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+	    $stateProvider
+	        .state('home', {
+		    url: '/home',
+		    templateUrl: '/home.html',
+		    controller: 'ToolsCtrl'
+		})
+	        .state('addActivity', {
+		    url: '/addActivity',
+		    templateUrl: '/addActivity.html',
+		    controller: 'ActivityCtrl as ctrl'
+		})
+	    $urlRouterProvider.otherwise('home')
+	}])
+    .controller('ActivityCtrl', function($scope) {
+	this.newActivity = function() {
+	    console.log('t')
+	}
+    })
+    .controller('ToolsCtrl', function($scope, $location) {
+	$scope.conditionalAdd = function() {
+	    var nextPage
+	    switch($scope.selectedIndex) {
+	    case 0:
+		nextPage = 'addActivity'
+		break
+	    case 1:
+		nextPage = 'addMood'
+		break
+	    case 2:
+		nextPage = 'addEvent'
+		break
+	    }
+	    $location.path(nextPage)
+	    console.log('A', nextPage)
+	}
+    })
