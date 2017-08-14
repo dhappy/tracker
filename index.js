4c736bd3d3f55da0245a8f011193b438fc83a5c5
Will Holcomb <will@dhappy.org>
2017-08-14 19:42:59 -0400

    working add

--- index.js
+++ index.js
@@ -7,31 +7,43 @@ angular.module('eventTypes', ['ngMaterial', 'ui.router'])
 	        .state('home', {
 		    url: '/home',
 		    templateUrl: 'home.html',
-		    controller: 'ToolsCtrl'
+		    controller: 'HomeController'
 		})
 	        .state('addActivity', {
 		    url: '/addActivity',
 		    templateUrl: 'addActivity.html',
-		    controller: 'ActivityCtrl as ctrl'
+		    controller: 'ActivityController as ctrl'
 		})
 	    $urlRouterProvider.otherwise('home')
 	}])
-    .controller('ActivityCtrl', function($scope, $location) {
+    .controller('HomeController', function($scope, $mdDialog, $location) {
 	$scope.activities = [{ name: 'ttt' }]
-	
+
 	this.newActivity = function() {
 	    $scope.activities.push({ name: 'test' }) 
 	}
 
-	this.cancel = function() {
-	    $location.path('home')
-	}
-    })
-    .controller('ToolsCtrl', function($scope, $location) {
-	$scope.conditionalAdd = function() {
+	$scope.conditionalAdd = function(event) {
 	    var nextPage
 	    switch($scope.selectedIndex) {
 	    case 0:
+		$mdDialog.show({
+		    title: 'What would you name your dog?',
+		    controller: DialogController,
+		    templateUrl: 'addActivity.html',
+		    parent: angular.element(document.body),
+		    targetEvent: event,
+		    clickOutsideToClose: true,
+		    fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+		})
+		    .then(function(value) {
+			console.log(value)
+			$scope.activities.push(value)
+		    },
+			  function() {
+			      $location.path('home')
+
+			  })
 		nextPage = 'addActivity'
 		break
 	    case 1:
@@ -41,7 +53,20 @@ angular.module('eventTypes', ['ngMaterial', 'ui.router'])
 		nextPage = 'addEvent'
 		break
 	    }
-	    $location.path(nextPage)
-	    console.log('A', nextPage)
+	    //$location.path(nextPage)
+	}
+
+	function DialogController($scope, $mdDialog) {
+	    $scope.hide = function() {
+		$mdDialog.hide()
+	    }
+
+	    $scope.cancel = function() {
+		$mdDialog.cancel()
+	    }
+
+	    $scope.returnNew = function() {
+		$mdDialog.hide({ name: $scope.name, color: $scope.color })
+	    }
 	}
     })
