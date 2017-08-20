f74a94dcb399538650863301ca9bc7a7b369bd83
Will Holcomb <will@dhappy.org>
2017-08-20 18:40:45 -0400

    switching to terms

--- index.js
+++ index.js
@@ -26,11 +26,24 @@ angular.module('eventTypes', ['ngMaterial', 'chart.js', 'ui.router', 'timer'])
     })
     .run(function(Activity) {})
 */
+    .controller('DialogController', function($scope, $mdDialog) {
+	$scope.hide = function() {
+	    $mdDialog.hide()
+	}
+	
+	$scope.cancel = function() {
+	    $mdDialog.cancel()
+	}
+	
+	$scope.returnNew = function() {
+	    $mdDialog.hide({ name: $scope.name, color: $scope.color })
+	}
+    })
     .controller('HomeController', function($scope, $mdDialog, $location) {
 	$scope.activities = [{ name: 'Testing', id: 1, lastEvent: new Date() },
 			     { name: 'Number Two', id: 2, lastEvent: new Date() },
 			     { name: 'Index Two', id: 3, lastEvent: new Date() }]
-	$scope.descriptors = [{ name: 'Testing', id: 1, lastEvent: new Date() }]
+	$scope.terms = [{ name: 'Testing', id: 1, lastEvent: new Date() }]
 	$scope.events = [
 	    { activity_id: 1, time: new Date() }
 	]
@@ -39,7 +52,7 @@ angular.module('eventTypes', ['ngMaterial', 'chart.js', 'ui.router', 'timer'])
 	    switch($scope.selectedTab) {
 	    case 0:
 		$mdDialog.show({
-		    controller: DialogController,
+		    controller: 'DialogController',
 		    templateUrl: 'addActivity.html',
 		    parent: angular.element(document.body),
 		    targetEvent: event,
@@ -55,16 +68,16 @@ angular.module('eventTypes', ['ngMaterial', 'chart.js', 'ui.router', 'timer'])
 		break
 	    case 1:
 		$mdDialog.show({
-		    controller: DialogController,
-		    templateUrl: 'addMood.html',
+		    controller: 'DialogController',
+		    templateUrl: 'addTerm.html',
 		    parent: angular.element(document.body),
 		    targetEvent: event,
 		    clickOutsideToClose: true,
-		    fullscreen: true // Only for -xs, -sm breakpoints.
+		    fullscreen: true
 		})
 		    .then(function(value) {
 			if(value.name) {
-			    $scope.desciptors.push(value)
+			    $scope.terms.push(value)
 			}
 		    },
 			  function() {})
@@ -73,11 +86,16 @@ angular.module('eventTypes', ['ngMaterial', 'chart.js', 'ui.router', 'timer'])
 		break
 	    }
 
-	    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"]
+	    $scope.display_time = function(time) {
+		return moment(time).format('H:mm:ss')
+	    }
+	    
+	    $scope.labels = ["January", "February", "March", "April",
+			     "May", "June", "July"]
 	    $scope.series = ['Series A', 'Series B']
 	    $scope.data = [
 		[65, 59, 80, 81, 56, 55, 40],
-		[28, 48, 40, 19, 86, 27, 90]
+		[28, 48, 40, 25, 86, 27, 90]
 	    ]
 	    $scope.onClick = function (points, evt) {
 		console.log(points, evt)
@@ -108,6 +126,10 @@ angular.module('eventTypes', ['ngMaterial', 'chart.js', 'ui.router', 'timer'])
 	    return $scope.activities.find(x => x.id === id)
 	}
 	
+	$scope.getTermById = function(id) {
+	    return $scope.terms.find(x => x.id === id)
+	}
+	
 	$scope.activitySelected = function(activity) {
 	    var now = new Date()
 	    activity.lastEvent = now
@@ -116,37 +138,36 @@ angular.module('eventTypes', ['ngMaterial', 'chart.js', 'ui.router', 'timer'])
 	    $scope.selectedTab = 2
 	}
 	
-	$scope.moodSelected = function(id) {
-	    var activity = $scope.getActivityById(id)
+	$scope.moodSelected = function(term) {
 	    $mdDialog.show({
-		controller: DialogController,
+		controller: RecordController,
 		templateUrl: 'recordMood.html',
 		parent: angular.element(document.body),
 		targetEvent: event,
 		clickOutsideToClose: true,
-		fullscreen: true // Only for -xs, -sm breakpoints.
+		fullscreen: true,
+		locals: {
+		    term: term
+		}
 	    })
 		.then(function(value) {
-		    if(value.name) {
-			$scope.desciptors.push(value)
-		    }
+		    $scope.events.push(value)
+		    $scope.selectedTab = 2
 		},
 		      function() {})
 	}
 
-	function DialogController($scope, $mdDialog) {
+	function RecordController($scope, $mdDialog, $controller, term) {
+	    $controller('DialogController', { $scope: $scope })
+
 	    $scope.weight = 0
+
+	    $scope.name = term.name
 	    
-	    $scope.hide = function() {
-		$mdDialog.hide()
-	    }
-
-	    $scope.cancel = function() {
-		$mdDialog.cancel()
-	    }
-
 	    $scope.returnNew = function() {
-		$mdDialog.hide({ name: $scope.name, color: $scope.color })
+		$mdDialog.hide({ term_id: term.id,
+				 weight: $scope.weight,
+				 time: new Date()})
 	    }
 	}
     })
