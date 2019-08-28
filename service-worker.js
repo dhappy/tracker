11d644c2a8dd4982f856f3db98f0b50a8201dd4b
Will Holcomb <will@dhappy.org>
2019-08-28 01:04:30 -0400

    changing caching strategy

--- service-worker.js
+++ service-worker.js
@@ -31,10 +31,10 @@ self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
     // CODELAB: Precache static resources here.
     evt.waitUntil(
-	caches.open(CACHE_NAME).then((cache) => {
-	    console.log('[ServiceWorker] Pre-caching index');
-	    return cache.addAll(FILES_TO_CACHE);
-	})
+  caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching index');
+      return cache.addAll(FILES_TO_CACHE);
+  })
     );
     self.skipWaiting();
 });
@@ -43,33 +43,39 @@ self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
     // CODELAB: Remove previous cached data from disk.
     evt.waitUntil(
-	caches.keys().then((keyList) => {
-	    return Promise.all(keyList.map((key) => {
-		if (key !== CACHE_NAME) {
-		    console.log('[ServiceWorker] Removing old cache', key);
-		    return caches.delete(key);
-		}
-	    }));
-	})
+  caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+    if (key !== CACHE_NAME) {
+        console.log('[ServiceWorker] Removing old cache', key);
+        return caches.delete(key);
+    }
+      }));
+  })
     );
     self.clients.claim();
 });
 
+
 self.addEventListener('fetch', (evt) => {
-    console.log('[ServiceWorker] Fetch', evt.request.url);
-    // CODELAB: Add fetch event handler here.
-    if (evt.request.mode !== 'navigate') {
-	// Not a page navigation, bail.
-	return;
-    }
-    console.log('E', evt);
-    evt.respondWith(
-	fetch(evt.request)
-	    .catch(() => {
-		return caches.open(CACHE_NAME)
-		    .then((cache) => {
-			return cache.match('offline.html');
-		    });
-	    })
-    );
+  console.log('[ServiceWorker] Fetch', evt.request.mode, evt.request.url);
+  // CODELAB: Add fetch event handler here.
+
+  // Cache all sucessful requests
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return fetch(evt.request)
+        .then((response) => {
+          // If the response was good, clone it and store it in the cache.
+          if(response.status === 200) {
+            console.log(`Served from Network: ${evt.request.url}`)
+            cache.put(evt.request.url, response.clone());
+          }
+          return response;
+        }).catch((err) => {
+          // Network request failed, try to get it from the cache.
+          console.log(`Served from Cache: ${evt.request.url}`)
+          return cache.match(evt.request);
+        });
+    })
+  );
 });
